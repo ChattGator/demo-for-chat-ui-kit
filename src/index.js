cb9359d36f725eb0daba80a6ec66772dd5b8a7a7
vatsal0601 <62330868+vatsal0601@users.noreply.github.com>
2022-08-26 19:25:50 +0530

    added config

--- src/index.js
+++ src/index.js
@@ -5,23 +5,21 @@ import { ChattGatorProvider } from "chattgator-chat-kit";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<ChattGatorProvider
-			value={{
-				projectConfig: {
-					projectId: "6303ce473c0028aeb8e79460",
-					projectSecret: "aWSocXyx_62V",
-				},
-				user: {
-					projectId: "6303ce473c0028aeb8e79460",
-					name: "Test User 2",
-					userId: "ajshdhd2",
-					avatar: "https://avatars.githubusercontent.com/u/80971056",
-					userName: "testtest",
-					bio: "Hey there, I'm using Chat app",
-				},
-			}}>
-			<App />
-		</ChattGatorProvider>
-	</React.StrictMode>
+	<ChattGatorProvider
+		value={{
+			projectConfig: {
+				projectId: "6303ce473c0028aeb8e79460",
+				projectSecret: "aWSocXyx_62V",
+			},
+			user: {
+				projectId: "6303ce473c0028aeb8e79460",
+				name: "Test User 2",
+				userId: "ajshdhd2",
+				avatar: "https://avatars.githubusercontent.com/u/80971056",
+				userName: "testtest",
+				bio: "Hey there, I'm using Chat app",
+			},
+		}}>
+		<App />
+	</ChattGatorProvider>
 );
