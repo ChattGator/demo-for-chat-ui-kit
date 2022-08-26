b0e69aa2b86e5d24f7823772e99f9f7c8d8f0954
vatsal0601 <62330868+vatsal0601@users.noreply.github.com>
2022-08-26 18:47:52 +0530

    one section redy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChattGatorProvider } from "chattgator-chat-kit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ChattGatorProvider
+			value={{
+				projectConfig: {
+					projectId: "6303ce473c0028aeb8e79460",
+					projectSecret: "aWSocXyx_62V",
+				},
+				user: {
+					projectId: "6303ce473c0028aeb8e79460",
+					name: "Test User 2",
+					userId: "ajshdhd2",
+					avatar: "https://avatars.githubusercontent.com/u/80971056",
+					userName: "testtest",
+					bio: "Hey there, I'm using Chat app",
+				},
+			}}>
+			<App />
+		</ChattGatorProvider>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
